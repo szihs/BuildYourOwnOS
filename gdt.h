bf7c9073036f9da7345733f582a20a96fa56f952
Harsh Aggarwal <harsh.ag@samsung.com>
2021-12-09 18:04:20 +0530

    working

--- gdt.h
+++ gdt.h
@@ -4,31 +4,32 @@
 #include "types.h"
 
 class GlobalDescriptorTable {
-    public:
-        class SegmentDescriptor {
-            private:
-                uint16_t limit_lo;
-                uint16_t base_lo;
-                uint8_t base_hi;
-                uint8_t type;
-                uint8_t flags_limit_hi;
-                uint8_t base_vhi;
-            public:
-                SegmentDescriptor(uint32_t base, uint32_t limit, uint8_t type);
-                uint32_t Base();
-                uint32_t Limit();
-        }__attribute__((packed));
+public:
+  class SegmentDescriptor {
+  private:
+    uint16_t limit_lo;
+    uint16_t base_lo;
+    uint8_t base_hi;
+    uint8_t type;
+    uint8_t flags_limit_hi;
+    uint8_t base_vhi;
 
-        SegmentDescriptor nullSegmentSelector;
-        SegmentDescriptor unusedSegmentSelector;
-        SegmentDescriptor codeSegmentSelector;
-        SegmentDescriptor dataSegmentSelector;
-        
-        GlobalDescriptorTable();
-        ~GlobalDescriptorTable();
+  public:
+    SegmentDescriptor(uint32_t base, uint32_t limit, uint8_t type);
+    uint32_t Base();
+    uint32_t Limit();
+  } __attribute__((packed));
 
-        uint16_t CodeSegmentSelector();
-        uint16_t DataSegmentSelector();
+  SegmentDescriptor nullSegmentSelector;
+  SegmentDescriptor unusedSegmentSelector;
+  SegmentDescriptor codeSegmentSelector;
+  SegmentDescriptor dataSegmentSelector;
+
+  GlobalDescriptorTable();
+  ~GlobalDescriptorTable();
+
+  uint16_t CodeSegmentSelector();
+  uint16_t DataSegmentSelector();
 };
 
 #endif
--- kernel.cpp
+++ kernel.cpp
@@ -1,31 +1,67 @@
-#include "types.h"
 #include "gdt.h"
+#include "types.h"
+
+#define SCREEN_W 80
+#define SCREEN_H 25
+
+/*
+ #if 1
+    VideoMemory[i] = (VideoMemory[i] & 0xFF00)| str[i];
+#else
+    c = (VideoMemory[i] & 0xFF00) >> 8;
+    c = (c & 0xF << 4) | (c & 0xF0 >> 4);
+    VideoMemory[i] = (c << 8)| str[i];
+#endif
+ */
 void printf(const char *str) {
-    uint16_t *VideoMemory = (uint16_t*)0xb8000;
-    uint8_t c;
-    for (uint32_t i = 0; str[i] != '\0'; i++) {
-        #if 1
-           VideoMemory[i] = (VideoMemory[i] & 0xFF00)| str[i];
-        #else
-           c = (VideoMemory[i] & 0xFF00) >> 8;
-           c = (c & 0xF << 4) | (c & 0xF0 >> 4);
-           VideoMemory[i] = (c << 8)| str[i];
-        #endif
+  static uint8_t x = 0, y = 0;
+  uint16_t *VideoMemory = (uint16_t *)(0xb8000);
+  uint8_t c;
+  for (uint32_t i = 0; str[i] != '\0'; i++) {
+
+    switch (str[i]) {
+    case '\n':
+      x = 0;
+      y++;
+      break;
+    default:
+      VideoMemory[SCREEN_W * y + x] =
+          (VideoMemory[SCREEN_W * y + x] & 0xFF00) | str[i];
+      x++;
+      break;
     }
+
+    if (x >= SCREEN_W) {
+      x = 0;
+      y++;
+    }
+    if (y >= SCREEN_H) {
+      for (y = 0; y < SCREEN_H; y++)
+        for (x = 0; x < SCREEN_W; x++)
+          VideoMemory[SCREEN_W * y + x] =
+              (VideoMemory[SCREEN_W * y + x] & 0xFF00) | ' ';
+
+      y = x = 0;
+    }
+  }
 }
 
-typedef void (*constructor) ();
+typedef void (*constructor)();
 extern "C" constructor start_ctors;
 extern "C" constructor end_ctors;
 
 extern "C" void callConstructors() {
-	for (constructor *i = &start_ctors; i != &end_ctors; i++) 
-		(*i)();
+  for (constructor *i = &start_ctors; i != &end_ctors; i++)
+    (*i)();
 }
 
 extern "C" void kernelMain(void *multiboot_structure, uint32_t magicnumber) {
-    printf("Hello ~ From Custom Kernel");
-    GlobalDescriptorTable gdt;
-    
-    while(1);
+  for (int i = 0; i < 26; i++) {
+    printf("Hello ~ From Custom Kernel\n");
+    printf("Hello Again ~ From Custom Kernel");
+  }
+  GlobalDescriptorTable gdt;
+
+  while (1)
+    ;
 }
